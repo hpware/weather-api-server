7115ae6f29af7182e92b33463812c3a9537f8b04
flash_user <hw@yuanhau.com>
2025-03-22 11:50:36 +0800

    Enhance code formatting and add Prettier for consistent styling; update API response handling and improve error messages

--- app/index.ts
+++ app/index.ts
@@ -1,12 +1,13 @@
 import fs from "node:fs";
 
 const apiKey = process.env.CWA_API;
-const requestElement = "Weather,Now,AirTemperature,RelativeHumidity,DailyHigh,DailyLow";
+const requestElement =
+  "Weather,Now,AirTemperature,RelativeHumidity,DailyHigh,DailyLow";
 const geoInfomation = "Coordinates,CountyName,TownName";
 const webServerPort = 3000;
 
 async function fetchAPI(location: string) {
-    const stopLocation = location;
+  const stopLocation = location;
   const req = await fetch(
     `https://opendata.cwa.gov.tw/api/v1/rest/datastore/O-A0001-001?Authorization=${apiKey}&format=JSON&WeatherElement=${requestElement}&GeoInfo=${geoInfomation}`,
     {
@@ -16,23 +17,38 @@ async function fetchAPI(location: string) {
       },
     },
   );
-    if (!req.ok) {
+  if (!req.ok) {
     throw new Error(`API request failed with status ${req.status}`);
-    }
+  }
   const res = await req.json();
   console.log(location);
   return res;
-} 
+}
 
 Bun.serve({
   port: webServerPort,
-    routes: {
+  routes: {
     "/weather/:loc": async (req) => {
-    const location = req.params.loc;
+      const location = req.params.loc;
       const data = await fetchAPI(location);
       return Response.json(data);
     },
-    "/*": Response.json({ message: "Not found" }, { status: 404 }),
+    "/weather/": Response.json(
+      {
+        message: "Please add the location at the end.",
+      },
+      {
+        status: 403,
+      },
+    ),
+    "/*": Response.json(
+      {
+        message: "Not found",
+      },
+      {
+        status: 404,
+      },
+    ),
   },
 });
 
